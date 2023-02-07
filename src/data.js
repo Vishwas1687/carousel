112b6c55a1912a9ee367cd18994cdd7560e51ca6
Vishwas1687 <gpvishwas1687@gmail.com>
2023-02-07 19:48:41 +0530

    added-images-as-url

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const data=[{
+url:'https://media.istockphoto.com/id/1382384282/photo/bangalore-or-bengaluru.jpg?s=612x612&w=0&k=20&c=6pxwL3JxNV2B_NZSLMZLhrSLqAbyCPlGuSZYKImpjKQ=',
+    name:'bangalore'
+},
+{
+    url:'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg',
+    name:'tree'
+},
+{
+   url:'https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320',
+   name:'nature'
+},
+{
+    url:'https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014__340.jpg',
+    name:'grass'
+},
+{
+    url:'https://cdn.esawebb.org/archives/images/screen/weic2216b.jpg',
+    name:'celestial'
+} 
+]
